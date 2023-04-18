db2adb0b4688c90d2b02daa04a5dc54d539d27e2
Egor Sidorov <lelikegor1@gmail.com>
2023-04-18 21:11:09 +0300

    rework locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    pass
 
 
 class LoginPageLocators:
@@ -17,3 +17,8 @@ class ProductPageLocators:
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     PRODUCT_NAME_IN_BASKET = (By.CSS_SELECTOR, "div.alertinner strong")
     SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alertinner")
+
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -5,12 +5,27 @@ from .pages.product_page import ProductPage
 num_promo = [num if num != 7 else pytest.param(num, marks=pytest.mark.xfail) for num in range(10)]
 
 
-@pytest.mark.parametrize('link', num_promo)
-def test_guest_can_add_product_to_basket(browser, link):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}"
+# @pytest.mark.parametrize('link', num_promo)
+# def test_guest_can_add_product_to_basket(browser, link):
+#     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_promo}"
+#     page = ProductPage(browser, link, timeout=10)
+#     page.open()
+#     page.should_be_product_on_page()
+#     page.go_to_product_page()
+#     page.solve_quiz_and_get_code()
+#     page.should_be_message_basket_total()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link, timeout=10)
     page.open()
-    page.should_be_product_on_page()
-    page.go_to_product_page()
-    page.solve_quiz_and_get_code()
-    page.should_be_message_basket_total()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link, timeout=10)
+    page.open()
+    page.go_to_login_page()
+
